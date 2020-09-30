f5e9e2cbb6df41286229c7c0e3977f1929d5ac21
Fynf <65299553+Fynf@users.noreply.github.com>
2020-09-30 22:44:42 +0200

    :sparkles: Copy-to-Clipboard Funktion
    :bug: H2-Konsole im frisch umbenannten local-Profil auch mal eingeschaltet
    :clown_face: H2-DB wird bei Start jetzt mit ein paar Beispielwerten befüllt

---
+++ src/main/resources/data.sql
@@ -0,0 +1,23 @@
+INSERT INTO food (id, food_name) VALUES (1,'Kartoffel');
+INSERT INTO food (id, food_name) VALUES (2,'Apfelmuß');
+INSERT INTO food (id, food_name) VALUES (3,'Broccoli');
+INSERT INTO food (id, food_name) VALUES (4,'Bananen');
+INSERT INTO food (id, food_name) VALUES (5,'Kaffee');
+INSERT INTO food (id, food_name) VALUES (6,'Frauen');
+INSERT INTO food (id, food_name) VALUES (7,'🤔');
+
+INSERT INTO modifier (id, modifier_name) VALUES (1,'-Eintopf');
+INSERT INTO modifier (id, modifier_name) VALUES (2,'suppe');
+INSERT INTO modifier (id, modifier_name) VALUES (3,'essenz');
+INSERT INTO modifier (id, modifier_name) VALUES (4,'auflauf');
+INSERT INTO modifier (id, modifier_name) VALUES (5,'schmelze');
+INSERT INTO modifier (id, modifier_name) VALUES (6,'-Gratin');
+INSERT INTO modifier (id, modifier_name) VALUES (7,' frittiert');
+
+INSERT INTO connector (id, connector_name) VALUES (1,', dazu ');
+INSERT INTO connector (id, connector_name) VALUES (2,' angemacht mit ');
+INSERT INTO connector (id, connector_name) VALUES (3,' und ');
+INSERT INTO connector (id, connector_name) VALUES (4,' an einer Creme aus ');
+INSERT INTO connector (id, connector_name) VALUES (5,' serviert mit ');
+INSERT INTO connector (id, connector_name) VALUES (6,', überbacken mit ');
+INSERT INTO connector (id, connector_name) VALUES (7,', dazu gibt es ');
